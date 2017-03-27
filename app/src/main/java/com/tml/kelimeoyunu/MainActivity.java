f9f57f11773ac5efb9517ff674f74c79326cc315
tahabayi <tahabayi@gmail.com>
2017-03-27 16:35:35 +0300

    Initial Commit

---
+++ app/src/main/java/com/tml/kelimeoyunu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tml.kelimeoyunu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
