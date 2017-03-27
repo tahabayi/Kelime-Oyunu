23b9bfe4407bad1f1baee818a39dbcef4fbe85a1
tahabayi <tahabayi@gmail.com>
2017-03-27 21:06:09 +0300

    Pull 14 question

--- app/src/main/java/com/tml/kelimeoyunu/MainActivity.java
+++ app/src/main/java/com/tml/kelimeoyunu/MainActivity.java
@@ -1,18 +1,55 @@
 package com.tml.kelimeoyunu;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    public int[] quesnums = new int[14];
+    public int[] queslimits = {186,186,186,185,189,185,179};
+    public Question[] questions= new Question[14];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent i = new Intent(this,PlaymainActivity.class);
-        i.putExtra("letternum",4);
-        startActivity(i);
+        Random r = new Random();
+        for(int i = 0;i<14;i++){
+            final int count = i;
+            quesnums[i] = r.nextInt(queslimits[i/2]);
+            if(i%2==1)
+                while(quesnums[i] == quesnums[i-1])
+                    quesnums[i] = r.nextInt(queslimits[i/2]);
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference().child(String.valueOf((int)(i/2)+4)).child(String.valueOf(quesnums[i]));
+            myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    boolean isDone = true;
+                    questions[count] = dataSnapshot.getValue(Question.class);
+                    for(int i=0;i<14;i++){
+                        if(questions[i]==null)
+                            isDone = false;
+                    }
+                    if(isDone)
+                        for(int i=0;i<14;i++)
+                            Log.d("asd",questions[i].toString());
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+        }
+
     }
 }
