6c78e241fe93f28017d1ae711a3f77522e8d041b
tahabayi <tahabayi@gmail.com>
2017-03-27 19:55:23 +0300

    Pull question from firebase

--- app/src/main/java/com/tml/kelimeoyunu/PlaymainActivity.java
+++ app/src/main/java/com/tml/kelimeoyunu/PlaymainActivity.java
@@ -2,19 +2,47 @@ package com.tml.kelimeoyunu;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by taha on 27/03/17.
  */
 
 public class PlaymainActivity extends Activity{
+
+    TextView questionTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playmain);
 
+        questionTextView = (TextView) findViewById(R.id.questionTextView);
+
         int letternum = getIntent().getIntExtra("letternum",4);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+        myRef = myRef.child(String.valueOf(letternum)).child("0");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Question q = dataSnapshot.getValue(Question.class);
+                questionTextView.setText(q.question+" ?");
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
